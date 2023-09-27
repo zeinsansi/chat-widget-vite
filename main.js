98d0316ea8c1f7e24f092f6854369a95ad5e0192
Zein <zeinkasar123@gmail.com>
2023-09-27 14:26:51 +0200

    v0.1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets.js";
+import { CLOSE_ICON, MESSAGE_ICON, styles } from "./assets-v0.1.js";
 
 class MessageWidget {
   constructor(position = "bottom-right") {
